4ad706ce92ace902159827089daf6150b928f63c
php1301 <luudanthanchet@gmail.com>
2020-10-01 10:46:22 +0700

    main.dart modified with provider

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './screens/place_detail_screen.dart';
+import './providers/great_places.dart';
+import './screens/add_place_screen.dart';
+import './screens/places_list_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => GreatePlaces(),
+      child: MaterialApp(
+        title: 'Greate Places App',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+          PlaceDetailScreen.routeName: (ctx) => PlaceDetailScreen(),
+        },
+      ),
+    );
+  }
+}
